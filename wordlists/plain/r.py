afa50d869b3e1784751fdf748cd0fbb9dc902712
George Marmaras <100040302+gmarma23@users.noreply.github.com>
2022-10-30 16:07:30 +0200

    Delete r.py

--- wordlists/plain/r.py
+++
@@ -1,9 +0,0 @@
-lines = []
-with open('english wordlist.txt', 'r') as f:
-    for line in f.readlines():
-        if '!' not in line:
-            lines.append(line)
-
-with open('english wordlist.txt', 'w') as f:
-    for l in lines:
-        f.write(l)
