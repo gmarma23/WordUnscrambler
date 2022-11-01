caf84df0236e1dbb203dfde53d47d74c88bf5764
gmarma23 <gmarma99@gmail.com>
2022-11-01 18:30:18 +0200

    Added comments

--- word_unscrambler.py
+++ word_unscrambler.py
@@ -2,7 +2,9 @@ from word import Word, InvalidChar, EmptyValidCharlist
 from wordlist import Wordlist
 import os
 
+
 class WordUnscrambler():
+    # Path to export txt file with multiple unscrambled words 
     DEFAULT_OUTPUT_PATH = os.path.join(Wordlist.ROOT_DIR, 'output')
 
     def __init__(self, custom_mapped_wordlist=None, valid_chars=None):
@@ -10,10 +12,12 @@ class WordUnscrambler():
         self.__valid_chars = valid_chars
 
     def __make_output_path(self):
+        # Utility function to create output path if not already present
         if not os.path.exists(self.DEFAULT_OUTPUT_PATH):
             os.makedirs(self.DEFAULT_OUTPUT_PATH)
 
     def unscramble_word(self, charlist):
+        # Unscramble single word from provided string of chars 
         try:
             word = Word(charlist, self.__valid_chars)
         except (InvalidChar, EmptyValidCharlist) as e:
@@ -24,19 +28,27 @@ class WordUnscrambler():
         return result
 
     def unscramble_words(self, input_path):
+        # Unscramble multiple words from input txt 
+        # file entries and export results in txt file  
         results = []
         try:
+            # Read input file entries 
             with open(input_path, 'r') as file:
+                # Parse file line by line
                 while (line := file.readline().rstrip()):
+                    # Append tuple (charlist, words) in results list
                     results.append((line, self.unscramble_word(line)))
         except FileNotFoundError:
             print('Invalid path to input file')
             return
 
+        # Extract filename from provided input txt file path 
         full_filename, ext = os.path.splitext(input_path)
         filename = f'{full_filename.split("/")[-1]} unscrambled{ext}'
         self.__make_output_path()
 
+        # Export results list in txt file in output directory 
+        # named "%input_file_name% unscrambled.txt"
         with open(os.path.join(self.DEFAULT_OUTPUT_PATH, filename), 'w') as file:
             for chars, words in results:
                 line = f'{chars} --> {words}\n'
--- wordlist.py
+++ wordlist.py
@@ -14,15 +14,22 @@ class Wordlist():
     
     @staticmethod
     def map_plain_wordlist(path_to_txt, valid_chars=None):
+        # Map plain wordlist entries to their corresponding integer signatures
+        # to a dictionary with signatures as keys and list of words as value  
         mapped_wordlist = {}
         try:
+            # Read wordlist entries from txt file
             with open(path_to_txt, 'r') as file:
+                # Parse file line by line
                 while (line := file.readline().rstrip()):
                     word = Word(line, valid_chars)
                     signature = word.get_int_signature()
                     if signature in mapped_wordlist.keys():
+                        # Signature already exists in dictionary, 
+                        # append new word in value list 
                         mapped_wordlist[signature].append(line)
                     else:
+                        # Create new key, value entry in dictionary
                         mapped_wordlist[signature] = [line] 
         except FileNotFoundError:
             print('Invalid path to plain wordlist')
@@ -31,12 +38,17 @@ class Wordlist():
             print(str(e))
             return
 
+        # Extract filename from given wordlist txt file path
         full_filename,_ = os.path.splitext(path_to_txt)
         filename = full_filename.split('\\')[-1]
+
+        # Export mapped wordlist dictionary as json file for future use
         with open(os.path.join(Wordlist.DEFAULT_MAPPED_DIR, f'{filename}.json'), 'w') as file:
             json.dump(mapped_wordlist, file)
 
     def __load_mapped_wordlist(self, custom_mapped_wordlist):
+        # Load mapped wordlist dictionary from json file
+        # Select default file if no custom one is provided
         mapped_wordlist_location = custom_mapped_wordlist if custom_mapped_wordlist else self.DEFAULT_MAPPED_WORDLIST
         try:
             with open(mapped_wordlist_location, 'r') as file:
@@ -46,6 +58,7 @@ class Wordlist():
             return
 
     def get_results(self, signature):
+        # Return list of matched words or empty list 
         signature = str(signature)
         if signature in self.__mapped_wordlist:
             return self.__mapped_wordlist[signature]
@@ -53,4 +66,6 @@ class Wordlist():
             return []
 
 
+# Module level alias for static function to avoid importing the whole class to call this function
+# Alternative Python solution to Java's "import static"
 map_plain_wordlist = Wordlist.map_plain_wordlist
